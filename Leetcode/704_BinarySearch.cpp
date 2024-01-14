5ea41c910a1cac6bb1b705d4645cd406ed29e636
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-01-14 23:27:47 +0500

    Daily dose of Leetcode

---
+++ Leetcode/704_BinarySearch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left =0,right= nums.size()-1;
+        while(left<=right){
+            int mid =  left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right = mid - 1 ;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
