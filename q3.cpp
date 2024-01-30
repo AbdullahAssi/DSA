029c907d58426aaf5b21f9b848f49b0908680809
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-01-30 14:56:51 +0500

    Add files via upload

--- q3.cpp
+++ q3.cpp
@@ -2,50 +2,46 @@
 using namespace std;
 
 class Graph{
-    private:
-        bool** adjMatrix;
-        int numVtx;
-        
-    public:
-        Graph(int numVtx){
-            this->numVtx = numVtx;
-            adjMatrix = new bool*[numVtx];
-            for(int i=0; i<numVtx; i++){
-                adjMatrix[i]= new bool[numVtx];
-                for(int j=0; j<numVtx; j++){
-                    adjMatrix[i][j]= false;
+	private:
+		bool** adjMatrix;
+		int numVtx;
+		
+	public:
+		Graph(int numVtx){
+			this->numVtx = numVtx;
+			adjMatrix = new bool*[numVtx];
+			for(int i=0; i<numVtx; i++){
+				adjMatrix[i]= new bool[numVtx];
+				for(int j=0; j<numVtx; j++){
+					adjMatrix[i][j]= false;
+				}
+			}
+			void addEdge(int i , int j){
+				adjMatrix[i][j]= true;
+				adjMatrix[j][i]= true;
+			}
+			void deleteEdge(int i , int j){
+				adjMatrix[i][j]= false;
+				adjMatrix[j][i]= false;
+			} 
+			
+			void toString(){
+                for(int i=0; i<numVtx; i++){
+                    cout<<i<<" : ";
+                    for(int j=0; j<numVtx; j++){
+                        cout<<adjMatrix[i][j]<<" ";
+                    }
+                    cout<<endl;
                 }
             }
-        }
-
-        void addEdge(int i , int j){
-            adjMatrix[i][j]= true;
-            adjMatrix[j][i]= true;
-        }
-
-        void deleteEdge(int i , int j){
-            adjMatrix[i][j]= false;
-            adjMatrix[j][i]= false;
-        } 
-            
-        void toString(){
-            for(int i=0; i<numVtx; i++){
-                cout<<i<<" : ";
-                for(int j=0; j<numVtx; j++){
-                    cout<<adjMatrix[i][j]<<" ";
+            ~Graph(){
+                for(int i=0; i<numVtx; i++){
+                    delete[] adjMatrix[i];
                 }
-                cout<<endl;
+                delete[] adjMatrix;
             }
-        }
-
-        ~Graph(){
-            for(int i=0; i<numVtx; i++){
-                delete[] adjMatrix[i];
-            }
-            delete[] adjMatrix;
-        }
+		}
 };
-
 int main(){
     Graph g(7);
     g.addEdge(0,2);
